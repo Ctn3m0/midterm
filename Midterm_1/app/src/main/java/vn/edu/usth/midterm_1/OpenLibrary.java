0e43803498dbc0d4f7c128bf9afdc3d84b2a6c85
vochidat0 <dat.usth2000@gmail.com>
2021-10-18 13:42:54 +0700

    merge main page

--- Midterm_1/app/src/main/java/vn/edu/usth/midterm_1/OpenLibrary.java
+++ Midterm_1/app/src/main/java/vn/edu/usth/midterm_1/OpenLibrary.java
@@ -10,8 +10,6 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;

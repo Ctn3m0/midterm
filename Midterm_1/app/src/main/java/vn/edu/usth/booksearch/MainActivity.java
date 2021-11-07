eda765b12a09c5541209b0a22d6ed0b8f6373d9a
unknown <aidenpearcewd01@gmail.com>
2021-11-07 23:57:33 +0700

    'subjects-api'

--- Midterm_1/app/src/main/java/vn/edu/usth/booksearch/MainActivity.java
+++ Midterm_1/app/src/main/java/vn/edu/usth/booksearch/MainActivity.java
@@ -3,14 +3,22 @@ package vn.edu.usth.booksearch;
 import androidx.appcompat.app.AppCompatActivity;
 import vn.edu.usth.booksearch.net.BookClient;
 import vn.edu.usth.booksearch.models.Book;
+import vn.edu.usth.booksearch.models.Category;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+
 import android.util.Log;
 import android.nfc.Tag;
 import android.widget.ProgressBar;
@@ -25,11 +33,13 @@ public class MainActivity extends AppCompatActivity {
 
     SearchView mySearchView;
     ListView myList;
-    private TextView mTextViewResult;
+    BufferedReader reader;
 
+    private TextView mTextViewResult;
     private BookClient client;
     private ProgressBar progress;
     private ArrayList<Book> aBooks;
+    private ArrayList<Category> _Cate;
     private boolean search;
 
     @Override
@@ -38,11 +48,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ArrayList<Book> aBooks = new ArrayList<Book>();
+        ArrayList<Category> _Cate = new ArrayList<Category>();
         mTextViewResult = findViewById(R.id.text_view_result);
         mTextViewResult.setText("123213");
 
+        List<String> _available_cate = new ArrayList<String>();
+
+        try{
+            reader = new BufferedReader(
+                    new InputStreamReader(getAssets().open("subjects")));
+            String line = reader.readLine();
+            while(line != null){
+                _available_cate.add(line);
+                line = reader.readLine();
+            }
+        } catch(IOException ioe){
+            ioe.printStackTrace();
+        }
+
+//      Read from File
+
+        for (int i = 0; i < _available_cate.size(); i++) {
+            Log.v("READ FROM FILE ",_available_cate.get(i));
+        }
+
+//      API TESTING
         search = false;
-        fetchBooks("Harry Potter", search);
+//        fetchBooks("Harry Potter", search);
+//        fetchCategory("architecture", search);
     }
 
     public void fetchBooks(String query, boolean search) {
@@ -59,11 +92,39 @@ public class MainActivity extends AppCompatActivity {
                         // Parse json array into array of model objects
                         final ArrayList<Book> books = Book.fromJson(docs);
                         // Remove all books from the adapter
-                        Log.v("Testing", "Hello");
                         for (Book book : books) {
                             Log.i("Data",book.getTitle());
-                            Log.i("Image",book.getCoverUrl());
                             mTextViewResult.setText(book.toString());
+                        }
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Log.e("ERROR",e.toString());
+                }
+            }
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                Log.v("BIG PROBLEM", "FAIL");
+            }
+        });
+    }
+
+    public void fetchCategory(String query, boolean search) {
+        this.search = search;
+        client = new BookClient();
+
+        client.getCategory(query, new JsonHttpResponseHandler() {
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                Log.v("fetch", query);
+                try {
+                    JSONArray works = null;
+                    if(response != null) {
+                        works = response.getJSONArray("works");
+                        // Parse json array into array of model objects
+                        final ArrayList<Category> _Cate = Category.fromJson(works);
+                        // Remove all books from the adapter
+                        Log.v("Testing", "Work");
+                        for (Category cate : _Cate) {
+                            Log.i("Category",cate.getCoverUrl());
                         }
                     }
                 } catch (JSONException e) {

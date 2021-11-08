efb76c80d93c0b909daa0906235a595476125313
Bell Kirato <Bell>
2021-11-08 14:02:56 +0700

    Adding search using API

--- Midterm_1/app/src/main/java/vn/edu/usth/midterm_1/OpenLibrary.java
+++ Midterm_1/app/src/main/java/vn/edu/usth/midterm_1/OpenLibrary.java
@@ -1,5 +1,7 @@
 package vn.edu.usth.midterm_1;
 
+import vn.edu.usth.midterm_1.models.Book;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
@@ -11,16 +13,30 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class OpenLibrary extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout mDrawerLayout;
@@ -35,7 +51,71 @@ public class OpenLibrary extends AppCompatActivity implements NavigationView.OnN
     private ImageView searchImage;
     private ImageView logoImage;
 
+
+    private vn.edu.usth.booksearch.net.BookClient client;
+    private boolean search;
+    private ImageButton Search;
+    private EditText input;
+
     private ActionBarDrawerToggle abdt;
+
+    public void fetchBooks(String query, boolean search) {
+        this.search = search;
+        client = new vn.edu.usth.booksearch.net.BookClient();
+        ListView listView = (ListView) findViewById(R.id.search_items);
+
+        client.getBooks(query, new JsonHttpResponseHandler() {
+            public void onSuccess(int statusCode, PreferenceActivity.Header[] headers, JSONObject response) {
+                Log.v("fetch", query);
+                try {
+                    JSONArray docs = null;
+                    if(response != null) {
+                        docs = response.getJSONArray("docs");
+                        // Parse json array into array of model objects
+                        final ArrayList<vn.edu.usth.midterm_1.models.Book> books = Book.fromJson(docs);
+                        // Remove all books from the adapter
+                        Log.v("Testing", "Hello");
+
+//                        List<Book> books = new List<>();
+
+                        ArrayList<String> books_info = new ArrayList<>();
+
+                        ArrayList<String> books_names = new ArrayList<>();
+                        ArrayList<String> books_authors = new ArrayList<>();
+                        ArrayList<Integer> books_covers = new ArrayList<>();
+
+                        for (vn.edu.usth.midterm_1.models.Book book : books) {
+                            books_info.add(book.getTitle()+" \n"+book.getAuthor() + " \n" + book.getPublisher());
+                            books_names.add(book.getTitle());
+                            books_authors.add(book.getAuthor());
+//                            books_covers.add(book.getCoverUrl());
+                            Log.i("Data",book.getTitle());
+                            Log.i("Image",book.getCoverUrl());
+
+                        }
+
+                        TextView headerTextView = new TextView(vn.edu.usth.midterm_1.OpenLibrary.this);
+                        headerTextView.setTypeface(Typeface.DEFAULT_BOLD);
+                        headerTextView.setText("List of Books");
+
+                        listView.addHeaderView(headerTextView);
+
+//                        CustomBookList customBookList = new CustomBookList(MainActivity.this, books_names, books_authors, books_covers);
+
+                        ArrayAdapter arrayAdapter = new ArrayAdapter(vn.edu.usth.midterm_1.OpenLibrary.this, android.R.layout.simple_list_item_1, books_info);
+
+                        listView.setAdapter(arrayAdapter);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Log.e("ERROR",e.toString());
+                }
+            }
+            public void onFailure(int statusCode, PreferenceActivity.Header[] headers, String responseString, Throwable throwable) {
+                Log.v("BIG PROBLEM", "FAIL");
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +123,23 @@ public class OpenLibrary extends AppCompatActivity implements NavigationView.OnN
         setContentView(R.layout.activity_main);
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.my_drawer_layout);
+        ArrayList<vn.edu.usth.midterm_1.models.Book> aBooks = new ArrayList<vn.edu.usth.midterm_1.models.Book>();
+
+
+        search = false;
+
+        input = (EditText) findViewById(R.id.search_input);
+        Search = (ImageButton) findViewById(R.id.search_button);
+        Search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fetchBooks(input.getText().toString(), search);
+                Toast.makeText(vn.edu.usth.midterm_1.OpenLibrary.this, input.getText().toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+
 
 
         abdt = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
